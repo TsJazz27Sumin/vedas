d5f7dac14fe86d9b109c602f01b2302fcf818bff
hasumiyouhei <hasumi.youhei@panair.jp>
2019-12-11 23:11:24 +0900

    path構成を変更

--- viewer/config/settings/base.py
+++ viewer/config/settings/base.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'config.urls'
+ROOT_URLCONF = 'viewer.config.urls'
 
 TEMPLATES = [
     {
@@ -71,7 +71,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'config.wsgi.application'
+WSGI_APPLICATION = 'viewer.config.wsgi.application'
 
 
 # Database
