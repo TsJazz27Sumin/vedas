d8bdeef5ba69804622bb337b892c57646ceac93a
hasumiyouhei <hasumi.youhei@panair.jp>
2019-12-11 22:08:03 +0900

    deploy対応

--- viewer/server/config/wsgi.py
+++ viewer/server/config/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'viewer.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'viewer.server.config.settings')
 
 application = get_wsgi_application()
