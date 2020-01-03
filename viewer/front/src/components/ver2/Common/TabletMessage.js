7e6af0682ced5d65933c14fbc0262cd06f83d085
hasumiyouhei <hasumi.youhei@panair.jp>
2020-01-03 11:41:11 +0900

    tablet対応：sorry画像を仕込んだ。

--- viewer/front/src/components/ver2/Common/TabletMessage.js
+++ viewer/front/src/components/ver2/Common/TabletMessage.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import ReactGA from 'react-ga';
+import styled from 'styled-components';
 import { AppProvider, Modal, TextContainer } from '@shopify/polaris';
 
 const TabletMessage = () => {
@@ -11,36 +12,58 @@ const TabletMessage = () => {
 
   const action = () => {window.history.back(-1)};
 
+  const getRandomInt = (max) => {
+    return Math.floor(Math.random() * Math.floor(max));
+  };
+  const public_url = process.env.PUBLIC_URL;
+  const ny_image = public_url + '/tablet/ny' + (getRandomInt(2) + 1) + '.JPG';
+
+  const ImageStyled = styled.img`
+    margin-top:10%;
+    width:100%;
+  `;
+
+  const ModalDiv = styled.div`
+    height: 500px;
+  `;
+
+  const BackGroundDiv = styled.div`
+    background: #000;
+  `;
+
   return (
-    <AppProvider>
-      <div style={{height: '500px'}}>
-        <Modal
-          open={true}
-          onClose={action}
-          title="Thank you for coming this site! (^^)"
-          primaryAction={{
-            content: 'Back',
-            onAction: action,
-          }}
-        >
-          <Modal.Section>
-            <TextContainer>
-              <p>
-                現在タブレットデバイスでは使用できません。 デスクトップPCまたはノートブックPCの使用をお勧めします。 モバイル版の機能は制限されています。
-              </p>
-              <br/>
-              <p>
-                But currently not available on tablet devices. We recommend using a desktop PC or notebook PC. The mobile version has limited functionality.
-              </p>
-              <br/>
-              <p>
-                但目前不适用于平板电脑设备。 我们建议使用台式机或笔记本电脑。 移动版本的功能有限。
-              </p>
-            </TextContainer>
-          </Modal.Section>
-        </Modal>
-      </div>
-    </AppProvider>
+    <BackGroundDiv>
+      <AppProvider>
+        <ImageStyled src={ny_image} alt="ny1"/>
+        <ModalDiv>
+          <Modal
+            open={true}
+            onClose={action}
+            title="Thank you for coming this site! (^^)"
+            primaryAction={{
+              content: 'Back',
+              onAction: action,
+            }}
+          >
+            <Modal.Section>
+              <TextContainer>
+                <p>
+                  現在タブレットデバイスでは使用できません。 デスクトップPCまたはノートブックPCの使用をお勧めします。 モバイル版の機能は制限されています。
+                </p>
+                <br/>
+                <p>
+                  But currently not available on tablet devices. We recommend using a desktop PC or notebook PC. The mobile version has limited functionality.
+                </p>
+                <br/>
+                <p>
+                  但目前不适用于平板电脑设备。 我们建议使用台式机或笔记本电脑。 移动版本的功能有限。
+                </p>
+              </TextContainer>
+            </Modal.Section>
+          </Modal>
+        </ModalDiv>
+      </AppProvider>
+    </BackGroundDiv>
   );
 }
 
