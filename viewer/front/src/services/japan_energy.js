ee324d07fec8eb4d58e4656991eda3718a60c689
hasumiyouhei <hasumi.youhei@panair.jp>
2020-01-05 20:51:44 +0900

    axiosについて書いた。

--- viewer/front/src/services/japan_energy.js
+++ viewer/front/src/services/japan_energy.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 const baseUrl = process.env.REACT_APP_API_BASE_URL + 'viewer/analyzer/'
 
 const get = (unit, from, to) => {
-  const request = axios.get(baseUrl + 'get?unit=' + unit + '&from=' + from + '&to=' + to)
+  const request = axios.get(baseUrl + 'get?unit=' + unit + '&from=' + from + '&to=' + to);
   return request.then(response => response.data).catch((err) => {
     console.error(err);
     return "err";
@@ -10,7 +10,7 @@ const get = (unit, from, to) => {
 }
 
 const get_daily_data = (unit, year, month, date) => {
-  const request = axios.get(baseUrl + 'get_daily_data?unit=' + unit + '&year=' + year + '&month=' + month + '&date=' + date)
+  const request = axios.get(baseUrl + 'get_daily_data?unit=' + unit + '&year=' + year + '&month=' + month + '&date=' + date);
   return request.then(response => response.data).catch((err) => {
     console.error(err);
     return "err";
