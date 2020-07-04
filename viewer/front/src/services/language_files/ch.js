a4395a75cdd5a262802e77432f1a1ea2e6cc7c87
hasumiyouhei <hasumi.youhei@panair.jp>
2020-07-04 12:25:11 +0900

    7月分の更新

--- viewer/front/src/services/language_files/ch.js
+++ viewer/front/src/services/language_files/ch.js
@@ -12,6 +12,7 @@ const get = () => {
         "analyze_condtion_text5": "※ 请选择您要检查的日期",
         "sample_news2": "(sample) January data has been updated.",
         "sample_news1": "(sample) Happy New Year. Vedas has been released.",
+        "news5_2": "仅东京电力公司2020/06年的数据，除北陆电力公司外，其他电力服务区2020/05年的数据。 更新至。",
         "news4_2": "北陆电力的数据更新至2020/02年，电力服务区域内其他地区的数据更新至2020/04年",
         "news3_2": "所有电力辖区的数据已更新至2020年01月",
         "news2_2": "所有电力辖区的数据已更新至2019年12月",
